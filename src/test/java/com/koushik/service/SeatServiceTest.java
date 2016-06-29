d4cbecc17fbe5b2ded4a4de119710f9233fed320
koushik R <koushik963@users.noreply.github.com>
2016-06-29 08:05:26 -0400

    Update SeatServiceTest.java

--- src/test/java/com/koushik/service/SeatServiceTest.java
+++ src/test/java/com/koushik/service/SeatServiceTest.java
@@ -104,47 +104,4 @@ public class SeatServiceTest {
         });
     }
 
-   // @Test
-    public void testRemoveExpiredSeatHolds() throws ParseException {
-        Date d1 = new Date();//new SimpleDateFormat().parse("06/28/2016 22:09:58");
-        Date d2 = new Date();//new SimpleDateFormat().parse("06/28/2016 22:10:58");
-        Date d3 = new Date();//new SimpleDateFormat().parse("06/28/2016 08:30:58");
-
-        SeatHold sh1 = new SeatHold();
-        sh1.setCustomerEmail("test@mock.com");
-        sh1.setHoldDate(d1);
-        sh1.setId(123);
-
-        SeatHold sh2 = new SeatHold();
-        sh1.setCustomerEmail("test@mock.com");
-        sh1.setHoldDate(d2);
-        sh1.setId(123);
-
-        Seat s1 = new Seat();
-        s1.setId(123456);
-        s1.setSeatNo(123);
-        s1.setStatus(SeatStatus.hold());
-
-        Seat s2 = new Seat();
-        s2.setId(123456);
-        s2.setSeatNo(123);
-        s2.setStatus(SeatStatus.hold());
-
-        sh1.setSeats(Arrays.asList(s1));
-        sh2.setSeats(Arrays.asList(s2));
-        Mockito.when(seatRepository.getCreatedSeatHoldObjects(d3)).thenReturn(Arrays.asList(sh1, sh2));
-        Mockito.when(seatRepository.saveSeat(s1)).thenReturn(null);
-        Mockito.when(seatRepository.saveSeat(s2)).thenReturn(null);
-        Mockito.when(Minutes.minutesBetween(new DateTime(sh1.getHoldDate()), new DateTime(d1)).getMinutes()).thenReturn(2);
-        Mockito.when(Minutes.minutesBetween(new DateTime(sh2.getHoldDate()), new DateTime(d2)).getMinutes()).thenReturn(2);
-        seatService.removeExpiredSeatHolds();
-        Arrays.asList(sh1,sh2).forEach(seatHold -> {
-            seatHold.getSeats().forEach(seat -> {
-                assertEquals(seat.getStatus(), SeatStatus.available());
-                assertEquals(seat.getSeatHold(), null);
-            });
-        });
-        Mockito.verify(seatRepository, Mockito.atLeastOnce()).removeSeatHold(sh1);
-        Mockito.verify(seatRepository, Mockito.atLeastOnce()).removeSeatHold(sh2);
-    }
 }
